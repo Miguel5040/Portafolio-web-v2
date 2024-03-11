feb8e5ed3491e21fb8897a838f868f045114448a
Miguel5040 <miguelssj5040@gmail.com>
2024-03-10 22:57:09 -0700

    Creacion del proyecto y Titulo principal agregado

---
+++ src/components/Hero.jsx
@@ -0,0 +1,12 @@
+import Title from "../sub-components/Title"
+
+const Hero = () => {
+
+    return (
+        <header className="flex justify-center pt-[110px] max-w-[880px]">
+            <Title />
+        </header>
+    )
+}
+
+export default Hero
---
+++ src/sub-components/Title.jsx
@@ -0,0 +1,10 @@
+
+const Title = () => (
+    <div>
+        <h1 className="font-nanum-myeongjo font-light text-[70px] text-balance text-center leading-[125%]">
+            Miguel Alberto es un desarrollador web ubicado en Los cabos, Mexico
+        </h1>
+    </div>
+)
+
+export default Title
