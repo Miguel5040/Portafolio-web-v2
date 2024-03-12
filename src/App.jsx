22c9f052906c96202b62e43a61644697f9aded17
Miguel5040 <miguelssj5040@gmail.com>
2024-03-12 16:50:03 -0700

    Seccion Sobre mi agregada y responsividad arreglada

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ const App = () => {
             <Hero />
             <Education />
             <Projects />
+            <About />
         </>
     )
 }
--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,15 +1,16 @@
+import Container from "../sub-components/Container"
 
 const About = () => (
-    <section className="flex flex-col lg:flex-row max-w-[1120px] mx-[16px] md:mx-[40px] lg:mx-[48px] xl:mx-[80px] mt-[80px] sm:mt-[100px] lg:mt-[120px] gap-10">
 
-        <div className="w-1/3 flex justify-start items-center">
-            <div className="max-w-[300px]">
-                <img className="aspect-square object-cover rounded-full border-2 border-[#333] p-2" src="./images/foto.jpg" alt="imagen de perfil" />
+    <Container className={"flex flex-col lg:flex-row lg:gap-0 gap-6 items-center"}>
+        <div className="lg:w-1/3 flex justify-start items-center">
+            <div className="max-w-[250px]">
+                <img className="aspect-square object-cover border-2 border-[#333] p-2" src="./images/foto.jpg" alt="imagen de perfil" />
             </div>
         </div>
         
-        <article className="w-2/3">
-            <div className="text-[30px]">
+        <article className="lg:w-2/3">
+            <div className="sm:text-[25px] text-[20px]">
                 <p className="mb-[30px]">
                     Apasionado por crear experiencias digitales únicas y funcionales. Mi enfoque se centra en la innovación y la creatividad para superar desafíos y ofrecer resultados excepcionales.
                 </p>
@@ -18,8 +19,7 @@ const About = () => (
                 </p>
             </div>
         </article>
-
-    </section>
+    </Container>
 )
 
 export default About
